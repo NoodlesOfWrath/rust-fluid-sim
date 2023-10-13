b748f4fd11eeeddd323e31d44b26bae6e8ac2fbb
Ethan Almloff <76850177+NoodlesOfWrath@users.noreply.github.com>
2023-10-13 08:49:19 -0500

    Fixed #1
    Circles were repulsing themselves causing unintended behavior.

--- src/main.rs
+++ src/main.rs
@@ -5,15 +5,18 @@ use rayon::prelude::*;
 use std::num;
 use std::time::Instant;
 
-const PARTICLECOUNT: u32 = 1000;
-const PARTICLEREPULSION: f32 = 0.005;
-const PARTICLEMAXREPULSION: f32 = 0.3;
-const PARTICLETERMINALVELOCITY: f32 = 100.0;
+const PARTICLECOUNT: u32 = 10000;
+const PARTICLEREPULSION: f32 = 0.15;
+const PARTICLEMAXREPULSION: f32 = 1.0;
+const PARTICLETERMINALVELOCITY: f32 = 10.0;
+const PARTICLESPHEREOFINFLUENCE: f32 = 100.0;
 
 //const BOXDIMS: [f32; 4] = [-100., 100., -300., -200.]; // Left, Right, Bottom, Top
 const BOXSIZE: [f32; 2] = [1200., 700.]; // Width, Height
 const BOXBOUNCINESS: f32 = 0.8;
-const GRAVITY: f32 = 0.0; //0.16; // 9.81 / 60.0;
+const BOXREPULSION: f32 = 0.3;
+const BOXMAXREPULSION: f32 = 1.0;
+const GRAVITY: f32 = 0.32; // 9.81 / 60.0;
 
 // Create a new type for a particle of water with velocity and position
 #[derive(Copy, Clone, Debug, Component)]
@@ -43,7 +46,7 @@ fn setup_circles(
         // Circle
         commands
             .spawn(MaterialMesh2dBundle {
-                mesh: meshes.add(shape::Circle::new(5.).into()).into(),
+                mesh: meshes.add(shape::Circle::new(2.).into()).into(),
                 material: materials.add(ColorMaterial::from(Color::BLUE)),
                 transform: Transform::from_translation(Vec3::new(0., 0., 0.)),
                 ..default()
@@ -79,6 +82,7 @@ fn initialize_positions(mut particleQuery: Query<&mut Particle, With<Particle>>)
 fn update(
     mut transfrom_query: Query<&mut Transform, With<Particle>>,
     mut particle_query: Query<&mut Particle>,
+    mut material_query: Query<&mut Handle<StandardMaterial>>,
 )
 // Called every frame
 {
@@ -110,7 +114,8 @@ fn set_circle_positions(
 
 fn simulation_step(particle: &mut Particle, other_particles: &Vec<Mut<'_, Transform>>) {
     gravity_step(particle);
-    repulsion_step(particle, other_particles);
+    particle_repulsion_step(particle, other_particles);
+    box_repulsion_step(particle);
     bounce_step(particle);
     cap_velocity(particle);
     apply_velocity_step(particle);
@@ -129,12 +134,24 @@ fn bounce_step(particle: &mut Particle) {
     }
 }
 
-fn repulsion_step(particle: &mut Particle, other_particles: &Vec<Mut<'_, Transform>>) {
+fn box_repulsion_step(particle: &mut Particle) {
+    let nearest_rectangle_point =
+        nearest_point_on_rectangle([particle.position.0, particle.position.1], BOXSIZE);
+    let dx = particle.position.0 - nearest_rectangle_point[0];
+    let dy = particle.position.1 - nearest_rectangle_point[1];
+    let distance = (dx * dx + dy * dy).sqrt();
+    // Check if the particle is not itself
+    particle.velocity.0 += (dx / distance.powi(2)).min(BOXMAXREPULSION) * BOXREPULSION as f32;
+    particle.velocity.1 += (dy / distance.powi(2)).min(BOXMAXREPULSION) * BOXREPULSION as f32;
+}
+
+fn particle_repulsion_step(particle: &mut Particle, other_particles: &Vec<Mut<'_, Transform>>) {
     other_particles.iter().for_each(|other_particle| {
         let dx = particle.position.0 - other_particle.translation.x;
         let dy = particle.position.1 - other_particle.translation.y;
         let distance = (dx * dx + dy * dy).sqrt();
-        if distance < 100. {
+        // Check if the particle is not itself
+        if distance != 0. && distance < PARTICLESPHEREOFINFLUENCE {
             particle.velocity.0 +=
                 (dx / distance.powi(2)).min(PARTICLEMAXREPULSION) * PARTICLEREPULSION as f32;
             particle.velocity.1 +=
@@ -151,4 +168,32 @@ fn apply_velocity_step(particle: &mut Particle) {
 fn cap_velocity(particle: &mut Particle) {
     particle.velocity.0 = particle.velocity.0.min(PARTICLETERMINALVELOCITY);
     particle.velocity.1 = particle.velocity.1.min(PARTICLETERMINALVELOCITY);
+}
+
+fn nearest_point_on_rectangle(point: [f32; 2], rectangle_size: [f32; 2]) -> [f32; 2] {
+    let half_width: f32 = rectangle_size[0] / 2 as f32;
+    let half_height: f32 = rectangle_size[1] / 2 as f32;
+
+    // Calculate distances to each edge
+    let left_distance = (point[0] + half_width).abs();
+    let right_distance = (point[0] - half_width).abs();
+    let top_distance = (point[1] + half_height).abs();
+    let bottom_distance = (point[1] - half_height).abs();
+
+    // Find the minimum of the four distances
+    let min_distance = left_distance
+        .min(right_distance)
+        .min(top_distance)
+        .min(bottom_distance);
+
+    // Return the nearest point and the distance
+    if min_distance == left_distance {
+        return [-half_width, point[1]];
+    } else if min_distance == right_distance {
+        return [half_width, point[1]];
+    } else if min_distance == top_distance {
+        return [point[0], -half_height];
+    } else {
+        return [point[0], half_height];
+    }
 }
